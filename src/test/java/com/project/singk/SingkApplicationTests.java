680a05ca9d9f544a6b3c3a41e8c3ae11ca821dfa
pmsu2007 <pmsu2007@naver.com>
2024-04-13 18:37:43 +0900

    feat : Spring Boot 프로젝트 생성
    Related to: SK-33

---
+++ src/test/java/com/project/singk/SingkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.singk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
