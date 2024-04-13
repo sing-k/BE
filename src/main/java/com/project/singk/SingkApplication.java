99dea5935488380b1fe9a63c939a0007821afbc5
pmsu2007 <pmsu2007@naver.com>
2024-04-13 18:37:43 +0900

    feat : Spring Boot 프로젝트 생성
    Related to: SK-33

---
+++ src/main/java/com/project/singk/SingkApplication.java
@@ -0,0 +1,13 @@
+package com.project.singk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingkApplication.class, args);
+	}
+
+}
