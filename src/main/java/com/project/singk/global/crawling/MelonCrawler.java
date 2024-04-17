553f2aba04733ced8780063dbef662ff3e87f8c8
pmsu2007 <pmsu2007@naver.com>
2024-04-17 21:23:43 +0900

    feat : 멜론 크롤러 모듈 구현 및 앨범 도메인 생성
    Base 크롤러를 기반으로한 멜론 전용 크롤러 구현
    - MelonCrawler.java
    Album 관련 데이터 관리를 위한 Domain 생성
    - Album.java
    - AlbumRequestDto.java
    - AlbumType.java
    Related to: SK-19

---
+++ src/main/java/com/project/singk/global/crawling/MelonCrawler.java
@@ -0,0 +1,51 @@
+package com.project.singk.global.crawling;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebElement;
+
+import com.project.singk.domain.album.dto.AlbumRequestDto;
+
+public class MelonCrawler {
+	private final String baseUrl = "https://www.melon.com/album/detail.htm?albumId=%d";
+	private Crawler crawler;
+	public MelonCrawler(Crawler crawler) {
+		this.crawler = crawler;
+	}
+
+	public AlbumRequestDto getAlbum(Long albumId) {
+		try {
+			crawler.setUrl(String.format(baseUrl, albumId));
+
+			// 앨범
+			WebElement album = crawler.getElement(By.cssSelector("#conts > div.section_info > div > div.entry"));
+
+			// 수록곡
+			List<String> tracks = new ArrayList<>();
+			List<WebElement> elements = crawler.getElements(By.className("wrap_song_info"));
+
+			for (WebElement e : elements) {
+				tracks.add(e.findElement(By.className("ellipsis")).getText());
+			}
+
+			return AlbumRequestDto.builder()
+				.melonId(albumId)
+				.imageUrl(crawler.getElement(By.cssSelector("#d_album_org > img")).getAttribute("src"))
+				.name(album.findElement(By.className("song_name")).getText())
+				.type(album.findElement(By.className("gubun")).getText())
+				.artist(album.findElement(By.className("artist_name")).getAttribute("title"))
+				.genre(album.findElement(By.cssSelector("div.meta > dl > dd:nth-child(4)")).getText())
+				.releasedAt(album.findElement(By.cssSelector("div.meta > dl > dd:nth-child(2)")).getText())
+				.tracks(tracks)
+				.build();
+
+		} catch (UnhandledAlertException e) {
+			e.getAlertText();
+		}
+
+		return null;
+	}
+}
