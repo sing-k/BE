a09a1ccd76786e79ac5c0c2dee754005e34fb8bb
pmsu2007 <pmsu2007@naver.com>
2024-04-17 16:50:53 +0900

    feat : 크롤러 모듈 구현
    확장 가능성이 있는 Base 크롤러 모듈 구현
    Related to : SK-19

---
+++ src/main/java/com/project/singk/global/crawling/Crawler.java
@@ -0,0 +1,44 @@
+package com.project.singk.global.crawling;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Crawler {
+	private static final String WEB_DRIVER_NAME = "webdriver.chrome.driver";
+	private static final String WEB_DRIVER_PATH = "./chromedriver";
+	private WebDriver driver;
+
+	public Crawler () {
+		// 크롬 드라이버 설정
+		System.setProperty(WEB_DRIVER_NAME, WEB_DRIVER_PATH);
+
+		// 크롬 드라이버 옵션 설정
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		options.addArguments("--disable-popup-blocking");
+
+		driver = new ChromeDriver(options);
+	}
+
+	public void setUrl(String url) {
+		driver.get(url);
+	}
+
+	public List<WebElement> getElements(By by) {
+			return driver.findElements(by);
+	}
+
+	public WebElement getElement(By by) {
+			return driver.findElement(by);
+	}
+
+	public void close() {
+		driver.close();
+	}
+}
