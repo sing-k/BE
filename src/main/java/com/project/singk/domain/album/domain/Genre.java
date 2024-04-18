ec2f4ff7e67f238450179ab8390b871ecdc869a3
pmsu2007 <pmsu2007@naver.com>
2024-04-18 18:19:35 +0900

    feat : Genre, AlbumGenre 엔티티 생성 및 연관관계 설정
    Related to: SK-19

--- src/main/java/com/project/singk/domain/album/domain/Genre.java
+++ src/main/java/com/project/singk/domain/album/domain/Genre.java
@@ -1,15 +1,10 @@
 package com.project.singk.domain.album.domain;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import com.project.singk.global.domain.BaseTimeEntity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,27 +21,15 @@ import lombok.ToString;
 @Getter @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "ALBUMS")
-public class Album extends BaseTimeEntity {
+@Table(name = "GENRES")
+public class Genre extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@Column(name = "melon_id")
-	private Long melonId;
-
 	private String name;
 
-	@Enumerated(EnumType.STRING)
-	private AlbumType type;
-
-	private String artist;
-
-	private String imageUrl;
-
-	private LocalDateTime releasedAt;
-
-	@OneToMany(mappedBy = "album", fetch = FetchType.LAZY)
+	@OneToMany(mappedBy = "genre")
 	List<AlbumGenre> albumGenres;
 }
