0bcd367380e7f2891d24e8ff78c10b27e3044127
pmsu2007 <pmsu2007@naver.com>
2024-04-23 18:51:39 +0900

    feat : 상태 코드 및 전역 예외 처리 추가
    - 상태 코드 추가
    - 유효성 검사 실패 시 발생하는 예외 처리
    Related to: SK-17

--- src/main/java/com/project/singk/global/api/AppHttpStatus.java
+++ src/main/java/com/project/singk/global/api/AppHttpStatus.java
@@ -9,15 +9,35 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AppHttpStatus {
 	/**
-	 * 200
+	 * 20X : 성공
 	 */
 	OK(200, HttpStatus.OK, "요청이 정상적으로 수행되었습니다."),
 	CREATED(201, HttpStatus.CREATED, "리소스를 생성하였습니다."),
 
 	/**
-	 * 403
+	 * 400 : 잘못된 문법으로 인해 요청을 이해할 수 없음
 	 */
-	FORBIDDEN(403, HttpStatus.FORBIDDEN, "권한이 없습니다.");
+	BAD_REQUEST(400, HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+	/**
+	 * 401 : 인증된 사용자가 아님
+	 */
+	UNAUTHORIZED(401, HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+
+	/**
+	 * 403 : 접근 권한이 없음
+	 */
+	FORBIDDEN(403, HttpStatus.FORBIDDEN, "권한이 없습니다."),
+
+	/**
+	 * 404 : 응답할 리소스가 없음
+	 */
+	NOT_FOUND(404, HttpStatus.FORBIDDEN, "존재하지 않는 리소스입니다."),
+
+	/**
+	 * 500 : 서버 내부에서 에러가 발생함
+	 */
+	INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에 에러가 발생했습니다.");
 
 	private final int statusCode;
 	private final HttpStatus httpStatus;
--- src/main/java/com/project/singk/global/api/GlobalExceptionHandler.java
+++ src/main/java/com/project/singk/global/api/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import java.nio.file.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import jakarta.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -17,4 +19,10 @@ public class GlobalExceptionHandler {
 	public BaseResponse<Void> accessDeniedExceptionHandler(AccessDeniedException e) {
 		return BaseResponse.fail(new ApiException(AppHttpStatus.FORBIDDEN));
 	}
+	@ExceptionHandler(ConstraintViolationException.class)
+	public BaseResponse<Void> constraintViolationExceptionHandler(ConstraintViolationException e) {
+		return BaseResponse.fail(new ApiException(AppHttpStatus.BAD_REQUEST));
+	}
+
+
 }
