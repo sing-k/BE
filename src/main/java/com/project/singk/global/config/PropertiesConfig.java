cf711a7d5b3bbaacb6812634fefaad724fd5faab
pmsu2007 <pmsu2007@naver.com>
2024-04-26 11:51:03 +0900

    feat : ConfigurationProperties 관련 설정 파일 생성
    com.project.sink 하위 패키지의 ConfigurationProperties 클래스를 찾아 등록
    Related to: SK-52

---
+++ src/main/java/com/project/singk/global/config/PropertiesConfig.java
@@ -0,0 +1,9 @@
+package com.project.singk.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = "com.project.singk")
+public class PropertiesConfig {
+}
