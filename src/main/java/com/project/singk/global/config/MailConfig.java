5a466a73a3abb37716f667858a739c93d6dc9085
pmsu2007 <pmsu2007@naver.com>
2024-04-28 20:13:04 +0900

    feat : 메일 설정 파일과 MailProperties에 Gmail 설정 추가
    - Properties 파일에 smtp 서버 주소, 포트 추가
    - SSL, TSL 연결 구분하는 로직 추가
    Related to: SK-52

--- src/main/java/com/project/singk/global/config/MailConfig.java
+++ src/main/java/com/project/singk/global/config/MailConfig.java
@@ -2,6 +2,7 @@ package com.project.singk.global.config;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -18,27 +19,42 @@ public class MailConfig {
 	private final MailProperties properties;
 
 	@Bean
-	public JavaMailSender naverMailService() {
+	@Qualifier("NaverMailSender")
+	public JavaMailSender naverMailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-		mailSender.setHost("smtp.naver.com");
+		mailSender.setHost(properties.getNaver().getHost());
 		mailSender.setUsername(properties.getNaver().getUsername());
 		mailSender.setPassword(properties.getNaver().getPassword());
-		mailSender.setPort(465);
+		mailSender.setPort(properties.getNaver().getPort());
 
-		mailSender.setJavaMailProperties(getNaverMailProperties());
+		mailSender.setJavaMailProperties(getMailProperties(true, false));
 
 		return mailSender;
 	}
 
-	private Properties getNaverMailProperties() {
+	@Bean
+	@Qualifier("GoogleMailSender")
+	public JavaMailSender googleMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+		mailSender.setHost(properties.getGoogle().getHost());
+		mailSender.setUsername(properties.getGoogle().getUsername());
+		mailSender.setPassword(properties.getGoogle().getPassword());
+		mailSender.setPort(properties.getGoogle().getPort());
+
+		mailSender.setJavaMailProperties(getMailProperties(false, true));
+
+		return mailSender;
+	}
+
+	private Properties getMailProperties(boolean isSSL, boolean isTLS) {
 		Properties properties = new Properties();
 		properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜
 		properties.setProperty("mail.smtp.auth", "true");
-		properties.setProperty("mail.smtp.starttls.enable", "true");
 		properties.setProperty("mail.debug", "true");
-		properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
-		properties.setProperty("mail.smtp.ssl.enable","true");
+		if (isSSL) properties.setProperty("mail.smtp.ssl.enable","true");
+		if (isTLS) properties.setProperty("mail.smtp.starttls.enable", "true");
 		return properties;
 	}
 }
