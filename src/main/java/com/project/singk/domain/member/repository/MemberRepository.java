a3fb0aba78dc2729f5f2694d581ac68f7ca1fe00
pmsu2007 <pmsu2007@naver.com>
2024-05-02 16:16:47 +0900

    feat : Service단 인증 코드 메일 전송 기능 구현
    - MemberRepository에 findByEmail 메소드 추가
    - MailProperties에 만료시간 추가
    - 메일 도메인 검증을 위한 MailDomain 생성
    - AppHttpStatus에 상태 코드 추가
    - 메일 전송 기능 구현
    Related to: SK-38

--- src/main/java/com/project/singk/domain/member/repository/MemberRepository.java
+++ src/main/java/com/project/singk/domain/member/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.project.singk.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.singk.domain.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findByEmail(String email);
 }
---
+++ src/main/java/com/project/singk/domain/member/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.project.singk.domain.member.service;
+
+import java.time.Duration;
+
+import org.springframework.stereotype.Service;
+
+import com.project.singk.domain.mail.domain.MailDomain;
+import com.project.singk.domain.mail.service.MailService;
+import com.project.singk.domain.member.domain.Member;
+import com.project.singk.domain.member.repository.MemberRepository;
+import com.project.singk.global.api.ApiException;
+import com.project.singk.global.api.AppHttpStatus;
+import com.project.singk.global.config.properties.MailProperties;
+import com.project.singk.global.util.RedisUtil;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthService {
+
+	private final String AUTH_PREFIX = "AuthCode";
+	private final MemberRepository memberRepository;
+	private final MailService mailService;
+	private final RedisUtil redisUtil;
+	private final MailProperties mailProperties;
+
+	public void sendAuthenticationCode(String email) {
+		// 이메일로 조회
+		Member member = memberRepository.findByEmail(email).orElse(null);
+		if (member != null) {
+			throw new ApiException(AppHttpStatus.DUPLICATE_MEMBER);
+		}
+
+		// TODO : 더 좋은 방법 찾기
+		String code;
+		if (MailDomain.GOOGLE.equals(email)) {
+			code = mailService.sendGoogleCertificationMail(email, mailProperties.getGoogle().getFrom());
+		} else if (MailDomain.NAVER.equals(email)) {
+			code = mailService.sendNaverCertificationMail(email, mailProperties.getNaver().getFrom());
+		} else {
+			throw new ApiException(AppHttpStatus.NOT_SUPPORT_EMAIL_FORMAT);
+		}
+
+		// 인증 번호 Redis 저장
+		redisUtil.setValue(AUTH_PREFIX + email, code, mailProperties.getExpirationMillis());
+	}
+}
