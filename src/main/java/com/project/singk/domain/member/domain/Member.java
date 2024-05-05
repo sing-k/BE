3832d74982bf77dcf0d5e3d3a958c566ec60eac7
pmsu2007 <pmsu2007@naver.com>
2024-05-06 01:24:50 +0900

    feat : Jwt 기반 Verification Filter 구현
    - SecurityConfig
    -- JwtVerificationFilter 등록
    - Member & SignupRequestDto
    -- Role 저장 로직 추가 및 테이블 명 추가
    Related to: SK-40

--- src/main/java/com/project/singk/domain/member/domain/Member.java
+++ src/main/java/com/project/singk/domain/member/domain/Member.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +22,7 @@ import lombok.ToString;
 @Getter @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "MEMBERS")
 public class Member extends BaseTimeEntity {
 
 	@Id
