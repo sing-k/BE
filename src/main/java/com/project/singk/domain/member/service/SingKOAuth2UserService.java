1526784b328ea38bd7bd161d68aaf2dbef871201
pmsu2007 <pmsu2007@naver.com>
2024-05-09 21:52:28 +0900

    feat : 커스텀 OAuth2UserService 구현
    - Security Config에 OAuth Service 등록
    Related to : SK-41

---
+++ src/main/java/com/project/singk/domain/member/service/SingKOAuth2UserService.java
@@ -0,0 +1,40 @@
+package com.project.singk.domain.member.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import com.project.singk.domain.member.domain.AuthType;
+import com.project.singk.domain.member.dto.oauth.GoogleResponse;
+import com.project.singk.domain.member.dto.oauth.KakaoResponse;
+import com.project.singk.domain.member.dto.oauth.NaverResponse;
+import com.project.singk.domain.member.dto.oauth.OAuthResponse;
+import com.project.singk.global.api.ApiException;
+import com.project.singk.global.api.AppHttpStatus;
+
+@Service
+public class SingKOAuth2UserService extends DefaultOAuth2UserService {
+
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+		System.out.println(oAuth2User);
+
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+		// TODO : 리팩토링
+		OAuthResponse oAuthResponse = null;
+		if (AuthType.GOOGLE.getId().equals(registrationId)) {
+			oAuthResponse = GoogleResponse.of(oAuth2User.getAttributes());
+		} else if (AuthType.NAVER.getId().equals(registrationId)) {
+			oAuthResponse = NaverResponse.of(oAuth2User.getAttributes());
+		} else if (AuthType.KAKAO.getId().equals(registrationId)) {
+			oAuthResponse = KakaoResponse.of(oAuth2User.getAttributes());
+		} else {
+			// 문제 가능성 있음
+			throw new ApiException(AppHttpStatus.INVALID_OAUTH_TYPE);
+		}
+	}
+}
