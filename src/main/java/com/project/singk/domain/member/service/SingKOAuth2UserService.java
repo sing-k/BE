beb4fd3bb7ebd52b00e72a67db0a466562ea0a4e
pmsu2007 <pmsu2007@naver.com>
2024-05-11 13:09:21 +0900

    feat : OAuth 로그인 회원 DB 저장
    - SingKOAuth2User 사용자 인증 정보 클래스 생성
    - SingKOAuth2UserService 회원 정보 저장 로직
    Related to : SK-41

--- src/main/java/com/project/singk/domain/member/service/SingKOAuth2UserService.java
+++ src/main/java/com/project/singk/domain/member/service/SingKOAuth2UserService.java
@@ -3,20 +3,28 @@ package com.project.singk.domain.member.service;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.project.singk.domain.member.domain.AuthType;
+import com.project.singk.domain.member.domain.Member;
+import com.project.singk.domain.member.domain.SingKOAuth2User;
 import com.project.singk.domain.member.dto.oauth.GoogleResponse;
 import com.project.singk.domain.member.dto.oauth.KakaoResponse;
 import com.project.singk.domain.member.dto.oauth.NaverResponse;
 import com.project.singk.domain.member.dto.oauth.OAuthResponse;
+import com.project.singk.domain.member.repository.MemberRepository;
 import com.project.singk.global.api.ApiException;
 import com.project.singk.global.api.AppHttpStatus;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class SingKOAuth2UserService extends DefaultOAuth2UserService {
 
+	private final MemberRepository memberRepository;
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		OAuth2User oAuth2User = super.loadUser(userRequest);
@@ -36,5 +44,12 @@ public class SingKOAuth2UserService extends DefaultOAuth2UserService {
 			// 문제 가능성 있음
 			throw new ApiException(AppHttpStatus.INVALID_OAUTH_TYPE);
 		}
+
+		Member member = memberRepository.findByEmail(oAuthResponse.getEmail()).orElse(null);
+		if (member != null) {
+			return SingKOAuth2User.of(oAuthResponse, false);
+		}
+
+		return SingKOAuth2User.of(oAuthResponse, true);
 	}
 }
