7d5e32b68f7432a137a854f3a5ad38061fd7671f
pmsu2007 <pmsu2007@naver.com>
2024-06-22 16:01:36 +0900

    feat : 서버 실행 시 어드민 계정 생성

---
+++ src/main/java/com/project/singk/global/scheduler/DataLoader.java
@@ -0,0 +1,36 @@
+package com.project.singk.global.scheduler;
+
+import com.project.singk.domain.member.domain.Member;
+import com.project.singk.domain.member.domain.MemberStatistics;
+import com.project.singk.domain.member.domain.Role;
+import com.project.singk.domain.member.service.port.MemberRepository;
+import com.project.singk.domain.member.service.port.PasswordEncoderHolder;
+import com.project.singk.global.properties.AdminProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements ApplicationRunner {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoderHolder passwordEncoderHolder;
+    private final AdminProperties adminProperties;
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+        if (memberRepository.existsByEmail(adminProperties.getEmail())) return;
+
+        // Admin 계정 생성
+        Member member = memberRepository.save(Member.builder()
+                .email(adminProperties.getEmail())
+                .password(passwordEncoderHolder.encode(adminProperties.getPassword()))
+                .nickname("관리자")
+                .role(Role.ROLE_ADMIN)
+                .statistics(MemberStatistics.empty())
+                .build());
+    }
+}
