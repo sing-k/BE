afaf2d1a7bfe85a0da1639df47e2f6f0acc165d8
pmsu2007 <pmsu2007@naver.com>
2024-07-12 17:26:51 +0900

    feat : 활동 히스토리 관련 기능 구현 - 활동 히스토리 그래프 조회 - 활동 히스토리 목록 조회

--- src/main/java/com/project/singk/domain/activity/service/ActivityHistoryServiceImpl.java
+++ src/main/java/com/project/singk/domain/activity/service/ActivityHistoryServiceImpl.java
@@ -1,11 +1,20 @@
 package com.project.singk.domain.activity.service;
 
 import com.project.singk.domain.activity.controller.port.ActivityHistoryService;
+import com.project.singk.domain.activity.controller.response.ActivityGraphResponse;
+import com.project.singk.domain.activity.controller.response.ActivityHistoryResponse;
+import com.project.singk.domain.activity.domain.ActivityHistory;
 import com.project.singk.domain.activity.service.port.ActivityHistoryRepository;
+import com.project.singk.global.api.PageResponse;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Service
 @Builder
@@ -14,6 +23,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class ActivityHistoryServiceImpl implements ActivityHistoryService {
 
     private final ActivityHistoryRepository activityHistoryRepository;
+    @Override
+    public List<ActivityGraphResponse> getActivityGraph(Long memberId) {
+        AtomicInteger accumulatedScore = new AtomicInteger(0);
+        return activityHistoryRepository.getActivityGraph(memberId).stream()
+                .map(activity -> {
+                    accumulatedScore.addAndGet(activity.getScore());
 
+                    return ActivityGraphResponse.builder()
+                            .date(activity.getCreatedAt())
+                            .score(accumulatedScore.get())
+                            .build();
+                })
+                .collect(Collectors.toList());
+    }
 
+    @Override
+    public PageResponse<ActivityHistoryResponse> getActivityHistories(Long memberId, int offset, int limit) {
+        Page<ActivityHistory> activityHistories = activityHistoryRepository.getActivityHistories(memberId, offset, limit);
+
+        return PageResponse.of(
+                offset,
+                limit,
+                (int) activityHistories.getTotalElements(),
+                activityHistories.getContent().stream()
+                        .map(ActivityHistoryResponse::from)
+                        .toList()
+        );
+    }
 }
--- src/main/java/com/project/singk/domain/activity/service/port/ActivityHistoryRepository.java
+++ src/main/java/com/project/singk/domain/activity/service/port/ActivityHistoryRepository.java
@@ -1,7 +1,12 @@
 package com.project.singk.domain.activity.service.port;
 
 import com.project.singk.domain.activity.domain.ActivityHistory;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface ActivityHistoryRepository {
     ActivityHistory save(ActivityHistory activityHistory);
+    List<ActivityHistory> getActivityGraph(Long memberId);
+    Page<ActivityHistory> getActivityHistories(Long memberId, int offset, int limit);
 }
