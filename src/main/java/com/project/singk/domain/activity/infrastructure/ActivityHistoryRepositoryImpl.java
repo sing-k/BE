8932236a4752e2b3707a5aff4a5bee5e63022c44
pmsu2007 <pmsu2007@naver.com>
2024-07-18 21:47:42 +0900

    feat : 활동 그래프 주별, 월별 기능 추가 및 함수 분리

--- src/main/java/com/project/singk/domain/activity/infrastructure/ActivityHistoryRepositoryImpl.java
+++ src/main/java/com/project/singk/domain/activity/infrastructure/ActivityHistoryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.project.singk.domain.activity.infrastructure;
 
+import com.project.singk.domain.activity.controller.request.ActivityDate;
 import com.project.singk.domain.activity.domain.ActivityHistory;
 import com.project.singk.domain.activity.domain.ActivityScore;
 import com.project.singk.domain.activity.service.port.ActivityHistoryRepository;
@@ -12,15 +13,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static com.project.singk.domain.activity.infrastructure.QActivityHistoryEntity.activityHistoryEntity;
-import static com.project.singk.domain.album.infrastructure.entity.QAlbumEntity.albumEntity;
-import static com.project.singk.domain.member.infrastructure.QMemberEntity.memberEntity;
-import static com.project.singk.domain.review.infrastructure.QAlbumReviewEntity.albumReviewEntity;
-import static java.util.stream.Collectors.groupingBy;
+
 
 @Repository
 @RequiredArgsConstructor
@@ -34,13 +33,23 @@ public class ActivityHistoryRepositoryImpl implements ActivityHistoryRepository 
     }
 
     @Override
-    public List<ActivityHistory> getActivityGraph(Long memberId) {
-        return queryFactory.selectFrom(activityHistoryEntity)
-                .where(activityHistoryEntity.member.id.eq(memberId))
-                .orderBy(activityHistoryEntity.createdAt.asc())
-                .fetch().stream()
+    public List<ActivityHistory> saveAll(List<ActivityHistory> activityHistories) {
+        return activityHistoryJpaRepository.saveAll(activityHistories.stream()
+                .map(ActivityHistoryEntity::from)
+                .toList()).stream()
                 .map(ActivityHistoryEntity::toModel)
-                .collect(Collectors.toList());
+                .toList();
+    }
+
+    @Override
+    public List<ActivityScore> getActivityGraph(Long memberId, LocalDate start, LocalDate end, ActivityDate type) {
+        // 누적합 구하기
+        List<ActivityHistory> accumulatedHistories = getAccumulatedHistories(memberId);
+
+        // 날짜 구하기
+        List<LocalDate> dates = getDates(start, end, type);
+
+        return getActivityScores(accumulatedHistories, dates);
     }
 
     @Override
@@ -65,6 +74,46 @@ public class ActivityHistoryRepositoryImpl implements ActivityHistoryRepository 
 
     @Override
     public List<ActivityScore> getActivityDailyGraph(Long memberId, LocalDate start, LocalDate end) {
+        // 누적합 구하기
+        List<ActivityHistory> accumulatedHistories = getAccumulatedHistories(memberId);
+
+        // 각 날짜별 최종 값 구하기
+        List<LocalDate> dates = start.datesUntil(end.plusDays(1)).toList(); // 호출 날짜 포함 7일
+
+        return getActivityScores(accumulatedHistories, dates);
+    }
+
+    @Override
+    public List<ActivityScore> getActivityWeeklyGraph(Long memberId, LocalDate start, LocalDate end) {
+        // 누적합 구하기
+        List<ActivityHistory> accumulatedHistories = getAccumulatedHistories(memberId);
+
+        // 각 주별 최종 값 구하기 - 각 주의 일요일
+        List<LocalDate> dates = new ArrayList<>(start.datesUntil(end.plusDays(1))
+                .filter(date -> date.getDayOfWeek() == DayOfWeek.SUNDAY)
+                .toList());
+
+        if (end.getDayOfWeek() != DayOfWeek.SUNDAY) dates.add(end);
+
+        return getActivityScores(accumulatedHistories, dates);
+    }
+
+    @Override
+    public List<ActivityScore> getActivityMonthlyGraph(Long memberId, LocalDate start, LocalDate end) {
+        // 누적합 구하기
+        List<ActivityHistory> accumulatedHistories = getAccumulatedHistories(memberId);
+
+        // 각 달별 최종 값 구하기 - 각 달의 마지막 날
+        List<LocalDate> dates = new ArrayList<>(start.datesUntil(end.plusDays(1))
+                .filter(date -> date.equals(YearMonth.from(date).atEndOfMonth()))
+                .toList());
+
+        if (!end.equals(YearMonth.from(end).atEndOfMonth())) dates.add(end);
+
+        return getActivityScores(accumulatedHistories, dates);
+    }
+
+    private List<ActivityHistory> getAccumulatedHistories(Long memberId) {
         List<ActivityHistory> activityHistories = queryFactory.select(activityHistoryEntity)
                 .from(activityHistoryEntity)
                 .where(activityHistoryEntity.member.id.eq(memberId))
@@ -81,11 +130,12 @@ public class ActivityHistoryRepositoryImpl implements ActivityHistoryRepository 
             accumulatedHistories.add(activityHistory.setAccumulatedScore(accumulateScore));
         }
 
-        // 각 날짜별 최종 값 구하기
-        List<ActivityScore> result = new ArrayList<>();
-        List<LocalDate> dates = start.datesUntil(end.plusDays(1)).toList(); // 호출 날짜 포함 7일
+        return accumulatedHistories;
+    }
 
+    private List<ActivityScore> getActivityScores(List<ActivityHistory> accumulatedHistories, List<LocalDate> dates) {
         int prev = 0;
+        List<ActivityScore> result = new ArrayList<>();
         for (LocalDate date : dates) {
             List<ActivityHistory> histories = accumulatedHistories.stream()
                     .filter(score -> score.getCreatedAt().isAfter(date.atStartOfDay()) && score.getCreatedAt().isBefore(date.plusDays(1).atStartOfDay()))
@@ -98,5 +148,31 @@ public class ActivityHistoryRepositoryImpl implements ActivityHistoryRepository 
         }
 
         return result;
+    }
+
+    private List<LocalDate> getDates(LocalDate start, LocalDate end, ActivityDate type) {
+        if (type.equals(ActivityDate.DAILY)) {
+            return start.datesUntil(end.plusDays(1)).toList();
+        } else if (type.equals(ActivityDate.WEEKLY)) {
+            // 각 주의 일요일
+            List<LocalDate> dates = new ArrayList<>(start.datesUntil(end.plusDays(1))
+                    .filter(date -> date.getDayOfWeek() == DayOfWeek.SUNDAY)
+                    .toList());
+
+            if (end.getDayOfWeek() != DayOfWeek.SUNDAY) dates.add(end);
+
+            return dates;
+        } else if (type.equals(ActivityDate.MONTHLY)) {
+            // 각 달의 마지막 날
+            List<LocalDate> dates = new ArrayList<>(start.datesUntil(end.plusDays(1))
+                    .filter(date -> date.equals(YearMonth.from(date).atEndOfMonth()))
+                    .toList());
+
+            if (!end.equals(YearMonth.from(end).atEndOfMonth())) dates.add(end);
+
+            return dates;
+        }
+
+        return null;
     }
 }
