bcf1594cd7c118d49d3fc5392431e9b79d25ef34
pmsu2007 <pmsu2007@naver.com>
2024-08-26 15:37:23 +0900

    fix : 알람 버그 임시 조치

--- src/main/java/com/project/singk/domain/alarm/controller/AlarmController.java
+++ src/main/java/com/project/singk/domain/alarm/controller/AlarmController.java
@@ -1,6 +1,8 @@
 package com.project.singk.domain.alarm.controller;
 
 import com.project.singk.domain.alarm.controller.port.AlarmService;
+import com.project.singk.domain.member.controller.port.AuthService;
+import com.project.singk.domain.member.controller.port.MemberService;
 import com.project.singk.global.api.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -13,17 +15,24 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class AlarmController {
 
     private final AlarmService alarmService;
+    private final AuthService authService;
 
     @GetMapping(value="/subscribe",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public BaseResponse<SseEmitter> subscribe(@RequestHeader(value="Last-Event-ID",required = false, defaultValue = "") String lastEventId){
-        // Todo: username-> (email) 받아오는 방법 고려
-        String username="email@email.com";
-        return BaseResponse.ok(alarmService.subscribe(username,lastEventId));
+    public BaseResponse<SseEmitter> subscribe(
+            @RequestHeader(value="Last-Event-ID", required = false, defaultValue = "") String lastEventId
+    ) {
+
+        return BaseResponse.ok(alarmService.subscribe(
+                authService.getLoginMemberId(),
+                lastEventId
+        ));
     }
 
     @DeleteMapping(value="/{alarmId}")
-    public BaseResponse<Void> delete(@PathVariable(value = "alarmId") Long id){
-        alarmService.delete(id);
+    public BaseResponse<Void> unsubscribe(
+            @PathVariable(value = "alarmId") Long alarmId
+    ) {
+        alarmService.unsubscribe(alarmId);
         return BaseResponse.ok();
     }
 }
